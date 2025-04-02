9eab2f28959212e1215dae3a8fc155976bfdb435
fdhhhdjd <nguyentientai10@gmail.com>
2025-04-02 14:26:01 +0700

    fix: new code

--- src/store/reducers/cartReducer.jsx
+++ src/store/reducers/cartReducer.jsx
@@ -6,33 +6,30 @@ const cartReducer = (state, action) => {
   const { type, payload } = action;
   switch (type) {
     case cartType.ADD_TO_CART:
-      const existingItem = state.cart.find(
-        (item) => item.id === action.payload.id
-      );
+      const existingItem = state.cart.find((item) => item.id === payload.id);
 
       if (existingItem) {
-        toast.success(`Just more 1 ${truncateTitle(action.payload.title, 23)}`);
+        toast.success(`Just more 1 ${truncateTitle(payload.title, 23)}`);
         return {
           ...state,
           cart: state.cart.map((item) =>
-            item.id === action.payload.id
+            item.id === payload.id
               ? { ...item, quantity: item.quantity + 1 }
               : item
           ),
         };
       }
-      toast.success(`Add to ${truncateTitle(action.payload.title, 23)}`);
+      toast.success(`Add to ${truncateTitle(payload.title, 23)}`);
       return {
         ...state,
-        cart: [...state.cart, { ...action.payload, quantity: 1 }],
+        cart: [...state.cart, { ...payload, quantity: 1 }],
       };
     case cartType.REMOVE_FROM_CART:
-      console.log(action);
       toast.success(`Deleted product successfully`);
 
       return {
         ...state, // Keep the rest of the state unchanged
-        cart: state.cart.filter((item) => item.id !== action.payload.id), // Update the cart
+        cart: state.cart.filter((item) => item.id !== payload.id), // Update the cart
       };
   }
 };
