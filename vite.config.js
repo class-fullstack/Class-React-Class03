ede5931c0435e838f6432f0840383e61a3c75e0f
fdhhhdjd <nguyentientai10@gmail.com>
2025-04-09 20:15:18 +0700

    feat: new code product

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: "dist",
+    // Đảm bảo Vite copy tệp _redirects và tệp khác từ thư mục public
+    assetsInlineLimit: 0,
+  },
+});
